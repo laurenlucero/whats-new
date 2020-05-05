4a1e62e7b579fa8f14e1ba990f42973a2b798563
“Lauren <lucerolaurenm@gmail.com>
2020-05-04 21:12:13 -0400

    Create NewsContainer component

--- src/components/NewsArticle/NewsArticle.js
+++ src/components/NewsArticle/NewsArticle.js
@@ -3,4 +3,18 @@ import './NewsArticle.css';
 
 // NEWSARTICLE COMPONENT CODE GOES HERE
 
+export class NewsArticle extends React.Component {
+    render() {
+        return (
+            <div style={{color: "white"}}>Hello World!</div>
+        )
+    }
+}
+
+// export const NewsArticle = () => {
+//     return (
+//         <div style={{ color: "white" }}>Hello World!</div> 
+//     )
+// }
+
 export default NewsArticle;
--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './NewsContainer.css'
+import { NewsArticle } from '../NewsArticle/NewsArticle'
 
 // NEWSCONTAINER COMPONENT CODE GOES HERE
+
+export const NewsContainer = () => {
+    return (
+    <NewsArticle />
+    )
+}
 
 export default NewsContainer;
