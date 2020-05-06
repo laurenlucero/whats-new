62e99ab61cae99d1f1e30a9fcf18e562c7488f0b
“Lauren <lucerolaurenm@gmail.com>
2020-05-06 11:09:30 -0400

    Add filter by article topic functionality

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Menu.css'
 
 // MENU COMPONENT CODE GOES HERE
+class Menu extends Component {
+    constructor({filterTopics}) {
+        super({filterTopics})
+    }
+
+    handleClick = (e) => {
+        console.log(e.target);
+        const {value} = e.target
+        this.props.filterTopics(value)
+    }
+
+    render() {
+    return (
+        <nav className='menu'>
+            <button value='local' className='nav-button' onClick={this.handleClick}>Local News</button>
+            <button value='technology' className='nav-button' onClick={this.handleClick}>Technology</button>
+            <button value='entertainment' className='nav-button' onClick={this.handleClick}>Entertainment</button>
+            <button value='science' className='nav-button' onClick={this.handleClick}>Science</button>
+            <button value='health' className='nav-button' onClick={this.handleClick}>Health</button>
+        </nav>
+    )
+    }
+}
 
 export default Menu;
